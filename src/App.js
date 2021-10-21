e3f89c4149b6cbad558d3cb37ca7e174017f871e
amartya <amartyaraj19412@gmail.com>
2021-10-20 20:20:09 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Permission from "./components/Permission";
+import City from './components/City'
+
+const App = () => {
+  return (
+    <>
+      <Permission /><br/><br/><br/>
+      <City />
+      <h1>hi</h1>
+    </>
+  );
+};
+export default App;
